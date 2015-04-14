dc5818804383c828dc5eed47e7fe25ce14a5c178
PG <pg2992@gmail.com>
2015-04-14 18:39:33 +0530

    Have setup grunt for bundling and minification of the JS files

---
+++ KnockOutValidation.js
@@ -0,0 +1,7 @@
+/*
+Concatinated JS filesAuthor : PawanCreated Date 2015-04-14
+ */ 
+
+$(function(){
+	console.log("Pawan");
+});
---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt){
+	//grunt related things go here
+	'use strict';
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+		concat : {
+			dist:{
+					options:{
+						separator : "\n\r",
+						banner:"/*\nConcatinated JS files"+
+							"Author : Pawan" +
+							'Created Date <%= grunt.template.today("yyyy-mm-dd") %>' +
+							"\n */ \n"
+					},
+
+				src : ["src/KnockValidation.js","src/extension.js"],
+				dest : 	"KnockOutValidation.js" 
+			}
+
+		},
+		uglify : {
+			options:{
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			build:{
+				src:"KnockOutValidation.js",
+				dest : "KnockOutValidation.min.js"
+
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+
+	grunt.registerTask('default',['concat','uglify']);
+
+
+};
